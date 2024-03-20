cab33e4ab6c67d4eb7b9436933fc04511044ff15
gaibarra <gaibarra@hotmail.com>
2024-03-20 11:26:12 -0600

    200324b

--- reportesmodelo/settings.py
+++ reportesmodelo/settings.py
@@ -161,4 +161,6 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "http://localhost:5173",
-    "http://localhost:3000",]
+    "http://localhost:3000",
+    "https://reportesmodelo.vercel.app",
+     ]
