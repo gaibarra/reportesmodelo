514aecb36349c3320b97d2e086a7cd076bae5263
gaibarra <gaibarra@hotmail.com>
2024-03-21 13:38:41 -0600

    210324d

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_BACKEND_URL
-    : "https://reportesmodelo-gaibarra.vercel.app/";
+    // : "https://reportesmodelo-gaibarra.vercel.app/";
+    : "http://localhost:8000";
 
 console.log(URL);
 const tasksApi = axios.create({
