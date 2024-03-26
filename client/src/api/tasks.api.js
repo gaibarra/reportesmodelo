2dcf0d1510f48edf53bb66f62202b8de8ec2c7f2
Gonzalo <gaibarra@hotmail.com>
2024-03-26 12:45:12 -0600

    260324e

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,12 +3,7 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_BACKEND_URL
-    : "http://127.0.0.1:8000";  
-
-console.log(URL);
-const tasksApi = axios.create({
-  baseURL: `${URL}/tasks/api/v1/tasks`,
-});
+    : "reportesmodelo.vercel.app";
 
 export const getAllTasks = () => tasksApi.get("/");
 
